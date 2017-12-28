7e7c77c1706bbcc55613b0912b2926568b0d4600
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-27 19:49:13 -0800

    created slide transition with css animation

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@
                     This is some info
                   </div>
                 </transition>
+                <transition name="slide">
+                  <div class="alert alert-info" v-if="show">
+                    This is some info
+                  </div>
+                </transition>
 
             </div>
         </div>
@@ -43,5 +48,39 @@
   .fade-leave-active {
     transition: opacity 1s;
     opacity: 0;
+  }
+
+  .slide-enter {
+
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 </style>
